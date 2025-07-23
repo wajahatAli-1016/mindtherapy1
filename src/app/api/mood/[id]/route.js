589c55dae0e7afc6f0f35446725fcd3d944a97c3
wajahatAli-1016 <wajahatalizaidi35@gmail.com>
2025-07-23 13:17:08 +0500

    Update route.js

--- src/app/api/mood/[id]/route.js
+++ src/app/api/mood/[id]/route.js
@@ -2,7 +2,8 @@ import {NextResponse} from 'next/server'
 import {getServerSession} from 'next-auth'
 import connectDB from '@/lib/db'
 import Mood from '@/models/Mood'
-import { authOptions } from '../auth/[...nextauth]/route';
+import { authOptions } from '../../auth/[...nextauth]/route';
+
 
 export async function GET(req){
     try{
